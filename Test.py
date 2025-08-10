40d03685e8fe2647b207282b560e3b65cfdb19d3
LAKKIREDDYPALLI ABHINAI <abhinailakki5@gmail.com>
2025-08-11 00:14:36 +0530

    Delete Test.py file

--- Test.py
+++
@@ -1,6 +0,0 @@
-from weather import get_weather_data
-
-city = input("Enter city name: ")
-temp, hum = get_weather_data(city)
-print(f"Temperature in {city}: {temp}°C")
-print(f"Humidity in {city}: {hum}%")
